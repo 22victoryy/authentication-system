bef8eadc09ccae2ac910a012a1c4b2c35c6bf8d6
vchanyoung <dk90801@gmail.com>
2019-09-12 23:34:52 -0400

    auth: config db

--- auth/server.js
+++ auth/server.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
+var mongoose = require('mongoose');
+
+//  need to add more featuees regarging server
+//  add dev stage
+
+//db config
+var db = require('../config/key').DBURI
+
+
+//db connection
+
+mongoose.connect(db, {useNewUrlParser: true})
+.then(() => console.log('mongodb connected'))
+.catch(err => console.log(err));
+
 
 var app = express();
 
 
+// ejs
 app.use(layouts);
 app.set('view engine', 'ejs');
+
+// bodyparser
+app.use(express.urlencoded({extended: false }));
 
 
 app.use('/', require('../routes/index'));
