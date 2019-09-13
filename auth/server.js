324d845a5bb4792eaf1ade724f4546263d0edcd9
vchanyoung <dk90801@gmail.com>
2019-09-13 02:05:57 -0400

    auth: add middleware for register

--- auth/server.js
+++ auth/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var session = require('express-session');
 
 //  need to add more featuees regarging server
 //  add dev stage
@@ -25,6 +27,38 @@ app.set('view engine', 'ejs');
 
 // bodyparser
 app.use(express.urlencoded({extended: false }));
+
+// Express session middleware
+// app.use(session({
+//   secret: 'secret',
+//   resave: true,
+//   saveUninitialized: true,
+// }));
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// connect flash
+app.use(flash());
+
+
+// app.use((req, res, next) => {
+//   res.locals.success_msg = req.flash('success_msg');
+//   res.locals.error_msg = req.flash('error_msg');
+//   next();
+// })
+
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  // res.locals.error = req.flash('error');
+  next();
+});
 
 
 app.use('/', require('../routes/index'));
