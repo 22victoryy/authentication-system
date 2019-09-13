586223fe54be32d40848ce9bb2c64e9e15965922
vchanyoung <dk90801@gmail.com>
2019-09-12 23:34:14 -0400

    routes: add user

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,34 @@ router.get('/Register', (req, res) =>
   res.render("Register")
 );
 
+// register handle
+router.post('/register', (req, res) => {
+  var { name, email, password, password2} = req.body;
+  // validation
+  let errors = [];
+
+  if (!name || !email || !password || !password2){
+    errors.push({msg: "pls enter everything"});
+  }
+
+  if (password != password2){
+    errors.push({msg: "pw does not match"});
+  }
+  if(password.length < 8) {
+    errors.push({msg: "pass less than 8 char"});
+  }
+  if (errors.length > 0){
+    res.render('register', {
+      errors,
+      name ,
+      email,
+      password,
+      password2
+    });
+  }
+  else {
+    res.send('pass');
+  }
+});
+
 module.exports = router;
