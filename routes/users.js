e2644c2031fd531c55eecba7d35ed577330c1234
vchanyoung <dk90801@gmail.com>
2019-09-13 02:07:11 -0400

    routes: fixed password hash, user existance validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs')
+// call user model
+const User = require('../model/usermodel');
 
 // Login 
 router.get('/Login', (req, res) =>
@@ -37,8 +40,57 @@ router.post('/register', (req, res) => {
     });
   }
   else {
-    res.send('pass');
+    // user validation
+    User.findOne({email: email})
+    .then(user => {
+      if (user)
+      {
+        errors.push({ msg: 'Email is already used to register.'})
+        res.render('register', {
+          errors,
+          name ,
+          email,
+          password,
+          password2
+        });
+      } else
+      {
+        var newuser = new User({
+          name,
+          email,
+          password,
+        });
+      console.log(newuser);
+      res.send('hello');
+      // password encryption
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newuser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newuser.password = hash;
+          newuser
+            .save()
+            .then(user => {
+              req.flash(
+                'success_msg',
+                'You are now registered and can log in'
+              );
+              res.redirect('/users/login');
+            })
+            .catch(err => console.log(err));
+          });
+        });
+      }
+    });
   }
 });
 
 module.exports = router;
+// if (err) throw err;
+// newuser.password = hash;
+// newuser.save()
+// .then(user => {
+//   req.flash('success', 'you are now registered');
+//   res.redirect('users/login');
+// })
+// .catch(err => console.log(err));
+// }));
